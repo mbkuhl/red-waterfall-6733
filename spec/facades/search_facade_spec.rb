cfa12b2654fdaf170a594ececd3da64234a580f2
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-15 10:44:40 -0700

    Add testing for facades

---
+++ spec/facades/search_facade_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe SearchFacade do
+  it "can create an array of character objects from API data" do
+    sf = SearchFacade.new
+    populace = sf.nation_index("fire+nation")
+    populace.each do |character|
+      expect(character).to be_a CharacterObject
+      expect(character.name).to_not be nil
+      expect(character.allies).to_not be nil
+      expect(character.enemies).to_not be nil
+      expect(character.affiliation).to_not be nil
+      expect(character.id).to_not be nil
+    end 
+  end
+end
