e3336f2dd88dbcdee5438c1bdc13ae24dcd9e836
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-15 10:35:25 -0700

    Add services testing:

---
+++ spec/services/service_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Service do
+  it "retrieves api details about a characters with a specific affiliation" do
+    service = Service.new
+    query = "?affiliation=fire+nation"
+    json = service.search_nation(query)
+    character = json.first
+
+    expect(json).to be_a Array
+    expect(character).to be_a Hash
+  
+    json.each do |char|
+      expect(char).to have_key :_id
+      expect(char[:_id]).to be_a(String)
+
+      expect(char).to have_key :name
+      expect(char[:name]).to be_a(String)
+
+      expect(char).to have_key :allies
+      expect(char[:allies]).to be_a(Array)
+
+      expect(char).to have_key :enemies
+      expect(char[:enemies]).to be_a(Array)
+
+      expect(char).to have_key :affiliation
+      expect(char[:affiliation]).to be_a(String)
+    end
+
+
+    expect(character).to have_key :photoUrl
+    expect(character[:photoUrl]).to be_a(String)
+  end
+end
